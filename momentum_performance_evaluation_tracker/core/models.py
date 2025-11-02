47678bb5a55c61e1dde79407fc8299e29afe7258
valbmg <21100904@usc.edu.ph>
2025-11-02 12:30:38 +0800

    refactor(core): migrate sql logic to django orms and forms

--- momentum_performance_evaluation_tracker/core/models.py
+++ momentum_performance_evaluation_tracker/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
+from django.core.exceptions import ValidationError
 
 class Role(models.Model):
     role_id = models.IntegerField(primary_key=True)
@@ -19,6 +20,10 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+    @classmethod
+    def email_exists(cls, email):
+        return cls.objects.filter(email_address__iexact=email).exists()
 
 class UserAccount(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='accounts')
@@ -26,7 +31,7 @@ class UserAccount(models.Model):
     password = models.CharField(max_length=128)
     role = models.ForeignKey(Role, on_delete=models.PROTECT)
     last_login = models.DateTimeField(null=True, blank=True, default=None)
-    is_first_login = models.BooleanField(default=True) # Track first login
+    is_first_login = models.BooleanField(default=True)
 
     def __str__(self):
         return self.username
@@ -36,6 +41,40 @@ class UserAccount(models.Model):
 
     def check_password(self, raw_password):
         return check_password(raw_password, self.password)
+
+    @classmethod
+    def username_exists(cls, username):
+        return cls.objects.filter(username__iexact=username).exists()
+    
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.objects.filter(username__iexact=username).first()
+    
+    @classmethod
+    def create_employee_user(cls, employee_data, username, password, role_id=303):
+        from django.db import transaction
+        
+        role = Role.objects.filter(pk=role_id).first()
+        if not role:
+            role, _ = Role.objects.get_or_create(
+                role_id=303, 
+                defaults={
+                    "role_name": "Employee", 
+                    "description": "Standard employee"
+                }
+            )
+        
+        with transaction.atomic():
+            employee = Employee.objects.create(**employee_data)
+            user = cls(
+                employee=employee,
+                username=username,
+                role=role
+            )
+            user.set_password(password)
+            user.save()
+        
+        return user
 
     # make models work with Django's auth
     @property
